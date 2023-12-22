5cb439ff41dc8be37b0813dce7a611e9e07ce94d
iris_spp <iris.song@derbysoft.net>
2023-12-22 13:53:28 +0800

    memo detail layout

--- lib/memo_info_tab.dart
+++ lib/memo_info_tab.dart
@@ -9,15 +9,68 @@ class MemoInfoTab extends StatefulWidget {
   }
 }
 
+const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+
 class _MemoInfoTabState extends State<MemoInfoTab> {
+  String dropdownValue = list.first;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Memo Detail'),
       ),
-      body: const Center(
-        child: Text('memo info'),
+      body: ListView(
+        children: <Widget>[
+          Row(
+            children: [
+              SizedBox(width: 100, child: Text('when')),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'when',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(width: 100, child: Text('type')),
+              Expanded(
+                  child: DropdownButton<String>(
+                value: dropdownValue,
+                icon: const Icon(Icons.arrow_downward),
+                items: list.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  // This is called when the user selects an item.
+                  setState(() {
+                    dropdownValue = value!;
+                  });
+                },
+              ))
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'what',
+                ),
+                maxLines: null,
+              ))
+            ],
+          ),
+          ElevatedButton(onPressed: () {}, child: Text('Submit'))
+        ],
       ),
     );
   }
